7ce831fde0472fa2046a1115bfe2e76cfd408fb0
Ryan Eno <ryan.eno@elastic.co>
2026-09-06 11:58:55 -0700

    Fix CI feature matrix and clippy failures

--- src/app.rs
+++ src/app.rs
@@ -903,14 +903,9 @@ impl App {
                     _ => unreachable!(),
                 };
 
-                match self
-                    .viewer
+                self.viewer
                     .flatjson
-                    .build_path_to_node(path_type, focused_row_index)
-                {
-                    Ok(path) => path,
-                    Err(err) => return Err(err),
-                }
+                    .build_path_to_node(path_type, focused_row_index)?
             }
         };
 
